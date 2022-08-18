b7a607a2984d0d4f08e93eedf528219e1090f118
Vanessa Mercado <vanemercado305@gmail.com>
2022-08-18 15:17:11 -0400

    se cambio el proyecto

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 let nombre = "Juan Perez";
-let edad = calcularEdad(2002)
+let edad = 25;
 console.log("Hola " + nombre);
 console.log(nombre + " tiene "+ edad + " años");
-
-function calcularEdad(anioNacimiento){
-    
-    return new Date().getFullYear() - anioNacimiento;
-}
