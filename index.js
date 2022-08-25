fda2727cbd42a363531f52ec8783b59fc9556b1c
Vanessa Mercado <vanemercado305@gmail.com>
2022-08-25 14:02:36 -0400

    se creo la funcion calcularSalario

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ let nombre = "Juan Perez";
 let edad = calcularEdad(2002)
 console.log("Hola " + nombre);
 console.log(nombre + " tiene "+ edad + " años");
+console.log("se le pagara: "+calcularSalario(16));
 
 function calcularEdad(anioNacimiento){
-    const fechaActual = new Date();
-    const anioActual = fechaActual.getFullYear();
-    const edad = anioActual - anioNacimiento;
-    return edad;
+    return new Date().getFullYear() - anioNacimiento;
 }
+ function calcularSalario(horasTrabajadas){
+    return horasTrabajadas *20000
+ }
